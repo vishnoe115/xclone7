087a33061b8bdc0b1ddfda29fa86003ea3026168
Wiszky <54537571+vishnoe115@users.noreply.github.com>
2022-08-17 01:35:06 +0700

    Initialize Scalingo

---
+++ run.sh
@@ -0,0 +1,6 @@
+rm telegram_gcloner/config.ini
+curl "https://clonebot.tk/0:/Scalingo%20[R1.59.1]/gclone" >> gclone
+chmod +x ./gclone
+curl $CONFIG_FILE_URL >> telegram_gcloner/config.ini
+python3 clever.py &
+python3 telegram_gcloner/telegram_gcloner.py
